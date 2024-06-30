91b35dab3c9e205de5ebde7f0d224bcd40065af8
Victor García Fernández <victorgf2011@gmail.com>
2024-07-01 00:00:23 +0200

    feat: functions and backtesting

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from prophet import Prophet
 import yfinance as yf
+import subprocess
 
 def download_stock_data(stock_name, start_date, end_date):
     stock = yf.Ticker(stock_name)
@@ -11,14 +12,14 @@ def download_stock_data(stock_name, start_date, end_date):
     df.columns = ['ds', 'y']
     # Remove timezone from date
     df['ds'] = df['ds'].dt.tz_localize(None)
+    # Fill missing dates with previous value
+    df = df.resample('D', on='ds').last().fillna(method='ffill').reset_index()
     return df
 
-def forecast_plot(forecast, df):
-    begining_last_3_months = df.iloc[-1]['ds'] - pd.DateOffset(months=3)
-    print(begining_last_3_months)
-
-    forecast_last_3_months = forecast.merge(df, on='ds', how='left')
-    forecast_last_3_months = forecast_last_3_months[forecast_last_3_months['ds'] > begining_last_3_months]
+def forecast_plot(forecast, df, filename = 'forecast.png'):
+    begining_last_months = df.iloc[-1]['ds'] - pd.DateOffset(months=3)
+    forecast_last_months = forecast.merge(df, on='ds', how='left')
+    forecast_last_months = forecast_last_months[forecast_last_months['ds'] > begining_last_months]
 
     fig, ax = plt.subplots()
     fig.subplots_adjust(right=0.75)
@@ -30,28 +31,39 @@ def forecast_plot(forecast, df):
     twin1.spines['right'].set_position(('outward', 60))
     twin2.spines['right'].set_position(('outward', 120))
 
-    p1, = ax.plot(forecast_last_3_months['ds'], forecast_last_3_months['yhat'], label='Predicted price', color='y')
-    p2, = twin1.plot(forecast_last_3_months['ds'], forecast_last_3_months['y'], label='Real price', color='b')
-    twin2.plot(forecast_last_3_months['ds'], forecast_last_3_months['yhat_upper'], label='Upper predicted price', color='g', alpha=0.5)
-    twin2.plot(forecast_last_3_months['ds'], forecast_last_3_months['yhat_lower'], label='Lower predicted price', color='g', alpha=0.5)
-    plt.fill_between(forecast_last_3_months['ds'], forecast_last_3_months['yhat_upper'], forecast_last_3_months['yhat_lower'], color='g', alpha=0.1)
+    p1, = ax.plot(forecast_last_months['ds'], forecast_last_months['yhat'], label='Predicted price', color='y')
+    p2, = twin1.plot(forecast_last_months['ds'], forecast_last_months['y'], label='Real price', color='b')
+    twin2.plot(forecast_last_months['ds'], forecast_last_months['yhat_upper'], label='Upper predicted price', color='g', alpha=0.5)
+    twin2.plot(forecast_last_months['ds'], forecast_last_months['yhat_lower'], label='Lower predicted price', color='g', alpha=0.5)
+    plt.fill_between(forecast_last_months['ds'], forecast_last_months['yhat_upper'], forecast_last_months['yhat_lower'], color='g', alpha=0.1)
 
     fig.legend(handles=[p1, p2])
     plt.gcf().autofmt_xdate()
     plt.gca().set_title('Stock price prediction AAPL')
-    fig.savefig('forecast.png')
+    fig.savefig(filename)
 
-m = Prophet()
+def backtest(stock, start_date, end_date):
+    m = Prophet()
 
-# . Download and prepare stock data
-df = download_stock_data('AAPL', '2021-01-01', '2024-3-01')
+    # Download and prepare stock data
+    df = download_stock_data(stock, '2000-01-01', end_date)
+    df.to_csv(f'{stock}.csv', index=False)
 
-# . Train the model
-df_without_last_3_months = df[df['ds'] < '2023-12-31']
-m.fit(df_without_last_3_months)
+    # Train the model
+    df_without_observable_part = df[df['ds'] < start_date]
+    m.fit(df_without_observable_part)
 
-# . Make predictions
-future = m.make_future_dataframe(periods=30)
-forecast = m.predict(future)
+    # Make predictions
+    periods = (pd.to_datetime(end_date) - pd.to_datetime(start_date)).days
+    future = m.make_future_dataframe(periods)
+    forecast = m.predict(future)
 
-forecast_plot(forecast, df)
+    filename = f'forecast_{stock}.png'
+    forecast_plot(forecast, df, filename)
+
+    subprocess.run(['open', filename])
+
+# En este pasa algo super extraño
+# backtest('AAPL', '2024-06-01', '2024-06-28')
+
+backtest('BIDU', '2024-05-01', '2024-07-28')
